fac9cdd857bf8d2eeae30176ab1b14690400b4ed
VAN DAMME Bojan (s) <bojan.van.damme@dt-c205-13.local>
2018-05-08 11:34:09 +0200

    Initial Commit

---
+++ Werkstuk 1Tests/Werkstuk_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Werkstuk_1Tests.swift
+//  Werkstuk 1Tests
+//
+//  Created by VAN DAMME Bojan (s) on 08/05/2018.
+//  Copyright © 2018 VAN DAMME Bojan (s). All rights reserved.
+//
+
+import XCTest
+@testable import Werkstuk_1
+
+class Werkstuk_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
