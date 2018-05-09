63682fbc08645d5e43c474c0df6c3193d5f9193e
VAN DAMME Bojan (s) <bojan.van.damme@dt-c205-21.local>
2018-05-09 16:57:39 +0200

    Tap op image met full view image

---
+++ Werkstuk 1/ImageViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ImageViewController.swift
+//  Werkstuk 1
+//
+//  Created by VAN DAMME Bojan (s) on 09/05/2018.
+//  Copyright © 2018 VAN DAMME Bojan (s). All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    var image:UIImage = UIImage()
+    
+    @IBOutlet weak var UIimg: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.UIimg.image = image
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Werkstuk 1/ItemViewController.swift
+++ Werkstuk 1/ItemViewController.swift
@@ -56,15 +56,20 @@ class ItemViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if let nextVC = segue.destination as? ImageViewController
+        {
+            nextVC.image = PersoonFoto.image!
+        }
+        
     }
-    */
+    
 
 }
